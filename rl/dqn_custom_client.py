f1396118fe5b5a8fec183b691a13aa54b97ad08c
typ4rk <typ4rk@gmail.com>
2019-07-15 23:05:27 +0900

    new(source): 최초 코드 업로드 (ver.4)

---
+++ rl/dqn_custom_client.py
@@ -0,0 +1,130 @@
+import setup_path
+from dqn_model import DQNClient
+from dqn_model import DQNParam
+import math
+from keras.layers import Dense
+from keras.optimizers import Adam
+from keras.models import Sequential
+import sys
+
+# =========================================================== #
+# Training finish conditions (hour)
+# assign training duration by hour : 0(limit less), 1 (an hour), 1.5 (an hour and half) ...
+# =========================================================== #
+training_duration = 0
+
+# =========================================================== #
+# model/weight load option
+# =========================================================== #
+model_load = False
+model_weight_path = "./save_model/.../dqn_weight_00.h5"
+
+
+# ===========================================================
+
+class DQNCustomClient(DQNClient):
+    def __init__(self):
+        self.dqn_param = self.make_dqn_param()
+        super().__init__(self.dqn_param)
+
+    # =========================================================== #
+    # Tuning area (Hyper-parameters for model training)
+    # =========================================================== #
+    @staticmethod
+    def make_dqn_param():
+        dqn_param = DQNParam()
+        dqn_param.discount_factor = 0.99
+        dqn_param.learning_rate = 0.00025
+        dqn_param.epsilon = 1.0
+        dqn_param.epsilon_decay = 0.999
+        dqn_param.epsilon_min = 0.01
+        dqn_param.batch_size = 100
+        dqn_param.train_start = 1000
+        dqn_param.memory_size = 20000
+        return dqn_param
+
+    # =========================================================== #
+    # Action Space (Control Panel)
+    # =========================================================== #
+    def action_space(self):
+        # =========================================================== #
+        # Area for writing code
+        # =========================================================== #
+        # Editing area starts from here
+        #
+        actions = [
+            dict(throttle=0.6, steering=0.1),
+            dict(throttle=0.6, steering=-0.1),
+            dict(throttle=0.6, steering=0.2),
+            dict(throttle=0.6, steering=-0.2),
+            dict(throttle=0.6, steering=0)
+        ]
+        #
+        # Editing area ends
+        # ==========================================================#
+        return actions
+
+    # =========================================================== #
+    # Reward Function
+    # =========================================================== #
+    def compute_reward(self, sensing_info):
+
+        # =========================================================== #
+        # Area for writing code
+        # =========================================================== #
+        # Editing area starts from here
+        #
+        thresh_dist = self.half_road_limit  # 4 wheels off the track
+        dist = abs(sensing_info.to_middle)
+
+        if dist > thresh_dist:
+            reward = -1
+        elif sensing_info.collided:
+            reward = -1
+        else:
+            if dist > 5:
+                reward = 0.1
+            elif dist > 4:
+                reward = 0.2
+            elif dist > 3:
+                reward = 0.4
+            elif dist > 2:
+                reward = 0.6
+            elif dist > 1:
+                reward = 0.8
+            else:
+                reward = 1
+
+        #
+        # Editing area ends
+        # ==========================================================#
+        return reward
+
+    # =========================================================== #
+    # Model network
+    # =========================================================== #
+    def build_custom_model(self):
+        model = Sequential()
+        model.add(Dense(32, input_dim=self.state_size, activation='relu',
+                        kernel_initializer='he_uniform'))
+        model.add(Dense(32, activation='relu',
+                        kernel_initializer='he_uniform'))
+        model.add(Dense(self.action_size, activation='linear',
+                        kernel_initializer='he_uniform'))
+        model.summary()
+
+        model.compile(loss='mse', optimizer=Adam(lr=self.dqn_param.learning_rate))
+
+        return model
+
+
+if __name__ == "__main__":
+    client = DQNCustomClient()
+
+    client.override_model()
+
+    if model_load:
+        client.agent.load_model(model_weight_path)
+
+    client.run(training_duration)
+    sys.exit()
